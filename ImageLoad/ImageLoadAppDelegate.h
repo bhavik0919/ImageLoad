316e683103b6ed5a0c8a5aa3efa418ce1c73a984
Amrita Gosh <Amrita@Amrita-Ghoshs-Mac-mini.local>
2011-05-17 16:53:18 +0530

    Initial Commit

---
+++ ImageLoad/ImageLoadAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ImageLoadAppDelegate.h
+//  ImageLoad
+//
+//  Created by Amrita Gosh on 17/05/11.
+//  Copyright 2011 Chakra Interactive Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageLoadAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
